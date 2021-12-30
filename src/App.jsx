c7fab7919f0a476c36bda267f4419289315a488a
Danny <danny@macbook-pro.myfiosgateway.com>
2021-12-30 01:48:42 -0500

    github search axios

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import List from './components/List'
 
 export default class App extends Component {
     
-    state = {users:[]}
+    state = {
+        users:[],           // users初始值為數組
+        isFirst: true,      // 是否為第一次打開頁面
+        isLoading: false,   // 標示是否處於加載中
+        err: '',            // 存儲請求相關的錯誤信息
+    }
 
-    saveUsers = (users) => {        
-        this.setState({users: users})
+    updateAppState = (stateObj) => {        
+        this.setState(stateObj)
     }
     
     render() {
-        const {users} = this.state;
-
         return (
             <div className="container">
-                <Search saveUsers={this.saveUsers}/>
-                <List users={users}/>
+                <Search updateAppState={this.updateAppState}/>
+                <List {...this.state}/>
             </div>
         )
     }
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -8,13 +8,19 @@ export default class Search extends Component {
         // 獲取用戶的輸入(連續解構賦值+重命名)
         const { keyWordElement:{value:keyWord} } = this;
         
+        // 發送請求前通知App更新狀態
+        this.props.updateAppState({isFirst:false, isLoading:true})
         // 發送網路請求
-        axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
+        axios.get(`https://api.github.com/search/usejjjrs?q=${keyWord}`).then(
             response => {
-                console.log('success', response.data)
-                this.props.saveUsers(response.data.items)
+                // 請求成功後通知App更新狀態
+                this.props.updateAppState({isLoading:false, users:response.data.items})
+                // this.props.saveUsers(response.data.items)
             },
-            error => {console.log('fail', error)}
+            error => {
+                //請求失敗後通知App更新狀態
+                this.props.updateAppState({isLoading:false, err: error.message})
+            }
         )
     }
     render() {
